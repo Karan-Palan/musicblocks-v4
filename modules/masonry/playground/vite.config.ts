497b43465a5328683cd8e77b5f6ffe9f6ff05f46
Karan Palan <karanpalan007@gmail.com>
2024-08-25 15:28:03 +0530

    build(masonry/playground): setup playground

--- modules/masonry/playground/vite.config.ts
+++ modules/masonry/playground/vite.config.ts
@@ -1,8 +1,18 @@
-/* eslint-disable prettier/prettier */
+import path from 'path';
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        //
+        react(),
+    ],
+
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, '..', 'src'),
+        },
+        extensions: ['.tsx', '.ts', '.js', '.scss', '.sass', '.json'],
+    },
 });
