e22225907df9340dcd8eeb5451d58c87ab0a0a47
Karan Palan <karanpalan007@gmail.com>
2024-09-02 16:06:44 +0530

    feat(masonry): Enable drag and drop for bricks and stack

--- modules/masonry/playground/pages/workspace/index.tsx
+++ modules/masonry/playground/pages/workspace/index.tsx
@@ -1,40 +1,11 @@
-import {
-  BrickBlock,
-  BrickData,
-  BrickExpression,
-  BrickStatement,
-  ModelBrickBlock,
-  ModelBrickData,
-  ModelBrickExpression,
-  ModelBrickStatement,
-} from '@/brick';
+import BrickFactory from './BrickFactory';
 import { WORKSPACES_DATA } from './data';
-import type { TBrickCoords, TBrickType } from '@/@types/brick';
 import type { Brick } from './data';
-
-function getBrick(
-  type: TBrickType,
-  instance: ModelBrickBlock | ModelBrickData | ModelBrickExpression | ModelBrickStatement,
-  coords: TBrickCoords,
-) {
-  switch (type) {
-    case 'data':
-      return <BrickData instance={instance as ModelBrickData} coords={coords} />;
-    case 'expression':
-      return <BrickExpression instance={instance as ModelBrickExpression} coords={coords} />;
-    case 'statement':
-      return <BrickStatement instance={instance as ModelBrickStatement} coords={coords} />;
-    case 'block':
-      return <BrickBlock instance={instance as ModelBrickBlock} coords={coords} />;
-    default:
-      return <></>;
-  }
-}
 
 function RenderBricks({ brickData }: { brickData: Brick }) {
   return (
     <>
-      {getBrick(brickData.type, brickData.instance, brickData.coords)}
+      <BrickFactory brickData={brickData} />
       {brickData.children &&
         brickData.children?.length > 0 &&
         brickData.children.map((child) => <RenderBricks key={child.id} brickData={child} />)}
@@ -44,14 +15,15 @@ function RenderBricks({ brickData }: { brickData: Brick }) {
 
 function WorkSpace() {
   return (
-    <div>
+    <div style={{ padding: '20px 50px' }}>
       {WORKSPACES_DATA.map((workspace) => (
         <svg
           version="1.1"
           xmlns="http://www.w3.org/2000/svg"
           key={workspace.id}
-          height="500px"
-          width="500px"
+          height="765px"
+          width="1800px"
+          style={{ border: '2px solid black' }}
         >
           {workspace.data.map((brick) => {
             return <RenderBricks key={brick.id} brickData={brick} />;
