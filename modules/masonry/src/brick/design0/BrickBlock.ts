dc630724d2faf1e1d8852b82b48bc0b41a2650d0
Karan Palan <karanpalan007@gmail.com>
2024-09-03 15:00:58 +0530

    feat(masonry): Add missing methods to BrickBlock.ts

--- modules/masonry/src/brick/design0/BrickBlock.ts
+++ modules/masonry/src/brick/design0/BrickBlock.ts
@@ -1,20 +1,20 @@
 import type { TBrickArgDataType, TBrickColor, TBrickCoords, TBrickExtent } from '@/@types/brick';
-
 import { BrickModelBlock } from '../model';
 import { generatePath } from '../utils/path';
 
-// -------------------------------------------------------------------------------------------------
-
 /**
- * @class
- * Final class that defines a block brick.
+ * Defines a block brick, extending `BrickModelBlock`.
  */
 export default class BrickBlock extends BrickModelBlock {
     readonly _pathResults: ReturnType<typeof generatePath>;
     readonly id: string;
+    readonly colorBgHighlight: TBrickColor;
+    readonly colorFgHighlight: TBrickColor;
+    public highlighted: boolean;
+    private _argExtents: Record<string, { argLengthX?: number; argLengthY: number }> = {};
+    private _folded: boolean;
 
     constructor(params: {
-        // intrinsic
         id: string;
         name: string;
         label: string;
@@ -22,31 +22,60 @@ export default class BrickBlock extends BrickModelBlock {
         args: Record<
             string,
             {
-                label: string;
-                dataType: TBrickArgDataType;
-                meta: unknown;
+                argId: string;
+                argLabel: string;
+                argTypeIncoming: TBrickArgDataType;
             }
         >;
         colorBg: TBrickColor;
         colorFg: TBrickColor;
         outline: TBrickColor;
+        colorBgHighlight: TBrickColor;
+        colorFgHighlight: TBrickColor;
         scale: number;
         connectAbove: boolean;
         connectBelow: boolean;
         nestLengthY: number;
+        folded?: boolean;
+        highlighted?: boolean;
     }) {
-        super(params);
+        super({
+            name: params.name,
+            label: params.label,
+            glyph: params.glyph,
+            args: Object.fromEntries(
+                Object.entries(params.args).map(([key, value]) => [
+                    key,
+                    {
+                        label: value.argLabel,
+                        dataType: value.argTypeIncoming,
+                        meta: {},
+                    },
+                ]),
+            ),
+            colorBg: params.colorBg,
+            colorFg: params.colorFg,
+            outline: params.outline,
+            scale: params.scale,
+            connectAbove: params.connectAbove,
+            connectBelow: params.connectBelow,
+        });
+
         this.id = params.id;
-        const argsKeys = Object.keys(this._args);
+        this.colorBgHighlight = params.colorBgHighlight;
+        this.colorFgHighlight = params.colorFgHighlight;
+        this.highlighted = params.highlighted ?? false;
+        this._folded = params.folded ?? false;
+
         this._pathResults = generatePath({
             hasNest: true,
             hasNotchArg: false,
-            hasNotchInsTop: this._connectAbove,
-            hasNotchInsBot: this._connectBelow,
-            scale: this._scale,
+            hasNotchInsTop: params.connectAbove,
+            hasNotchInsBot: params.connectBelow,
+            scale: params.scale,
             nestLengthY: params.nestLengthY,
             innerLengthX: 100,
-            argHeights: Array.from({ length: argsKeys.length }, () => 17),
+            argHeights: Array(Object.keys(params.args).length).fill(17),
         });
     }
 
@@ -68,10 +97,8 @@ export default class BrickBlock extends BrickModelBlock {
     }
 
     public get bBoxArgs(): Record<string, { extent: TBrickExtent; coords: TBrickCoords }> {
-        const argsKeys = Object.keys(this._args);
         const result: Record<string, { extent: TBrickExtent; coords: TBrickCoords }> = {};
-
-        argsKeys.forEach((key, index) => {
+        Object.keys(this._args).forEach((key, index) => {
             result[key] = {
                 extent: {
                     width: this._pathResults.bBoxArgs.extent.width * this._scale,
@@ -83,7 +110,6 @@ export default class BrickBlock extends BrickModelBlock {
                 },
             };
         });
-
         return result;
     }
 
@@ -135,6 +161,80 @@ export default class BrickBlock extends BrickModelBlock {
             coords: {
                 x: this._pathResults.bBoxNotchInsNestTop!.coords.x * this._scale,
                 y: this._pathResults.bBoxNotchInsNestTop!.coords.y * this._scale,
+            },
+        };
+    }
+
+    public get instantiationProperties(): {
+        id: string;
+        name: string;
+        label: string;
+        glyph: string;
+        args: Record<
+            string,
+            {
+                argId: string;
+                argLabel: string;
+                argTypeIncoming: TBrickArgDataType;
+            }
+        >;
+        colorBg: TBrickColor;
+        colorFg: TBrickColor;
+        colorBgHighlight: TBrickColor;
+        colorFgHighlight: TBrickColor;
+        outline: TBrickColor;
+        scale: number;
+        connectAbove: boolean;
+        connectBelow: boolean;
+        highlighted: boolean;
+        argExtents: Record<string, { argLengthX?: number; argLengthY: number }>;
+        folded: boolean;
+    } {
+        return {
+            id: this.id,
+            name: this.name,
+            label: this.label,
+            glyph: this.glyph,
+            args: Object.fromEntries(
+                Object.entries(this._args).map(([key, value]) => [
+                    key,
+                    {
+                        argId: value.label,
+                        argLabel: value.label,
+                        argTypeIncoming: value.dataType,
+                    },
+                ]),
+            ),
+            colorBg: this.colorBg,
+            colorFg: this.colorFg,
+            colorBgHighlight: this.colorBgHighlight,
+            colorFgHighlight: this.colorFgHighlight,
+            outline: this.outline,
+            scale: this._scale,
+            connectAbove: this.connectAbove,
+            connectBelow: this.connectBelow,
+            highlighted: this.highlighted,
+            argExtents: this._argExtents,
+            folded: this._folded,
+        };
+    }
+
+    public get renderProperties(): {
+        boundingBox: { extent: TBrickExtent; coords: TBrickCoords };
+        connectionPoints: {
+            Top: { extent: TBrickExtent; coords: TBrickCoords } | null;
+            Bottom: { extent: TBrickExtent; coords: TBrickCoords } | null;
+            TopInner: { extent: TBrickExtent; coords: TBrickCoords } | null;
+            ArgsIncoming: { extent: TBrickExtent; coords: TBrickCoords } | null;
+        };
+    } {
+        return {
+            boundingBox: this.bBoxBrick,
+            connectionPoints: {
+                Top: this.bBoxNotchInsTop,
+                Bottom: this.bBoxNotchInsBot,
+                TopInner: this.bBoxNotchInsNestTop,
+                ArgsIncoming: this.bBoxNotchArg,
             },
         };
     }
